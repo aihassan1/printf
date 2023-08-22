8e3af13e5f037397a4c11a2772e52e0a77c775f5
aihassan1 <abdosruor57@gmail.com>
2023-08-22 04:41:26 +0000

    test21

--- _printf.c
+++ _printf.c
@@ -2,86 +2,101 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
 /**
-* _printf - A customized printf function that supports %c and %s specifiers
-* @format: Format string
-* @...: Additional arguments.
-* Return: Number of characters which was printed.
-*/
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-int i, chars_printed_counter = 0, onechar, number;
-char *string = NULL;
-char buf[1024];
-va_list args;
-va_start(args, format);
+  int i, chars_printed_counter = 0, onechar, number;
+  char *string = NULL;
+  va_list args;
+  va_start(args, format);
 
-/* Handling case1 where format string is NULL  */
-if (format == NULL)
-{
-write(1, "(null)", strlen("(null)"));
-return (-1);
-}
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-/* Handle %c specifier */
-if (format[i + 1] == 'c')
-{
-onechar = va_arg(args, int);
-write(1, &onechar, sizeof(char));
-i += 1;
-chars_printed_counter++;
-}
-/* Handle %s specifier  */
-else if (format[i + 1] == 's')
-{
-string = va_arg(args, char*);
-if (string == NULL)
-{
-/* Handle NULL string  */
-write(1, "(null)", strlen("(null)"));
-i += 1;
-chars_printed_counter += strlen("(null)");
-}
-else
-{
-write(1, string, strlen(string));
-i += 1;
-chars_printed_counter += strlen(string);
-}
-}
-else if (format[i + 1] == '%')
-{
-write(1, "%", sizeof(char));
-i += 1;
-chars_printed_counter++;
-}
+  /* Handle case where format string is NULL */
+  if (format == NULL)
+    {
+      write(1, "(null)", strlen("(null)"));
+      return (-1);
+    }
 
+  for (i = 0; format[i] != '\0'; i++)
+    {
+      if (format[i] == '%')
+	{
+	  if (format[i + 1] == 'c')
+	    {
+	      /* Handle %c specifier */
+	      onechar = va_arg(args, int);
+	      write(1, &onechar, sizeof(char));
+	      i += 1;
+	      chars_printed_counter++;
+	    }
+	  else if (format[i + 1] == 's')
+	    {
+	      /* Handle %s specifier */
+	      string = va_arg(args, char*);
 
-/* Handle unrecognized specifier */
-else
-{
-write(1, &format[i], 2);
-i++;
-chars_printed_counter += 2;
-}
-}
-else
-{
-/* Write non-specifier character */
-onechar = format[i];
-write(1, &onechar, sizeof(char));
-chars_printed_counter++;
-}
-}
-va_end(args);
-if (chars_printed_counter < 0)
-{
-/* Handle overflow */
-return (-1);
-}
-return (chars_printed_counter);
+	      if (string == NULL)
+		{
+		  /* Handle NULL string */
+		  write(1, "(null)", strlen("(null)"));
+		  i += 1;
+		  chars_printed_counter += strlen("(null)");
+		}
+	      else
+		{
+		  write(1, string, strlen(string));
+		  i += 1;
+		  chars_printed_counter += strlen(string) ;
+		}
+	    }
+	  else if (format[i + 1] == '%')
+	    {
+	      /* Handle %% specifier */
+	      write(1, "%", sizeof(char));
+	      i += 1;
+	      chars_printed_counter++;
+	    }
+
+	  else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+	    {
+	      number = va_arg(args, int);
+	      char buf[1024];
+	      sprintf(buf, "%d", number);
+	      write(1, buf, strlen(buf));
+	      i += 1;
+	      chars_printed_counter++;
+	    }
+
+	  else
+	    {
+	      /* Handle unrecognized specifier */
+	      write(1, &format[i], 2);
+	      i++;
+	      chars_printed_counter += 2;
+	    }
+
+	}
+
+      else
+	{
+	  /* Write non-specifier character */
+	  onechar = format[i];
+	  write(1, &onechar, sizeof(char));
+	  chars_printed_counter++;
+	}
+    }
+
+  va_end(args);
+
+  if (chars_printed_counter < 0)
+    {
+      /* Handle overflow */
+      return -1;
+    }
+
+  return chars_printed_counter;
 }
