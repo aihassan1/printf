326ca9bd33ec838c7cd9a07d8bcf4509bcbb63e0
mostafaelrkhawy7 <mostafaelrkhawy43@gmail.com>
2023-08-22 11:39:01 -0700

    update headr file 1

--- main.h
+++ main.h
@@ -21,12 +21,12 @@ int _int_to_string(char *buf, size_t bufsize, int n);
 int _printf(const char *format, ...);
 int get_flags(const char *format, int *idx);
 int get_width(const char *format, int *idx, va_list list);
+int get_size(const char *format, int *idx);
 
 int is_digit(char);
 int _strlen(char *s);
 int is_printable(char);
 int append_hexa_code(char, char[], int);
-int handle_print(const char *ft, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 long int convert_size_unsgnd(unsigned long int numb1, int size);
 long int convert_size_number(long int numb, int size);
